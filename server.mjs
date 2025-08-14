0baaf2e5bbd94ab7d1c5bb7f87965ffa60510454
Soami Kapadia <kapadiasoami@gmail.com>
2025-08-13 22:40:16 -0700

    change port to not conflict with other services

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import { validateDiagramSyntax } from './validator.mjs';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.use(express.json({ limit: '256kb' }));
 
